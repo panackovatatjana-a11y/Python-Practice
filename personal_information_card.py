34337731bb4a82313fe6b527387bb50c34066782
panackovatatjana-a11y <panackovatatjana@gmail.com>
2026-08-18 14:49:04 +0100

    Add files via upload

---
+++ personal_information_card.py
@@ -0,0 +1,12 @@
+# Personal Information Card
+
+name = "TANA"
+age = 26
+city = "Calne"
+hobby = "Reiki & Coding"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Hobby:", hobby)
+
