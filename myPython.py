941512e529813d639bc5346025f27847214d36c9
panackovatatjana-a11y <panackovatatjana@gmail.com>
2026-08-18 13:00:06 +0100

    Add files via upload

---
+++ myPython.py
@@ -0,0 +1,17 @@
+# Product Price Checker
+# This program compares the prices of two products
+# and prints the name of the cheaper one.
+
+# 1. Create variables named after the products
+ferrari = 420000
+toyota = 21000
+
+# 2. Conditional statement to check which product is cheaper
+if ferrari < toyota:
+    print("ferrari is less expensive")
+else:
+    print("toyota is less expensive")
+
+
+
+
